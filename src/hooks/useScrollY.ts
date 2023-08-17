2734373dc999485cc6eb1222fc6dcfe4a5bae77a
Voy7 <89422331+Voy7@users.noreply.github.com>
2023-08-17 03:35:23 -0500

    Custom hook fix

--- src/hooks/useScrollY.ts
+++ src/hooks/useScrollY.ts
@@ -2,11 +2,10 @@ import { useState, useEffect } from 'react'
 
 // Custom hook to get the current scrollY position, 0 - 1
 export default function useScrollY() {
-  if (typeof window === 'undefined') return 0
-
-  const [scrollY, setScrollY] = useState<number>(window.scrollY / (document.body.scrollHeight - window.innerHeight)) // 0 - 1
+  const [scrollY, setScrollY] = useState<number>(0) // 0 - 1
 
   useEffect(() => {
+    setScrollY(window.scrollY / (document.body.scrollHeight - window.innerHeight))
     const handleScroll = () => setScrollY(window.scrollY / (document.body.scrollHeight - window.innerHeight))
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
